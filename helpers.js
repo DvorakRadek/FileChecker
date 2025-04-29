da148c466db59c6c2125053d04bb1256903cd1db
DvorakRadek <radekdvorak90@gmail.com>
2025-04-29 22:44:11 +0200

    updated

--- helpers.js
+++ helpers.js
@@ -1,5 +1,5 @@
 export const printHelpAndExit = () => {
-  console.error(`Usage:\ncheck-files 'path/to/your/directory' 'path/to/directory to save the result file' 'file extension of files to search' 'searched expressions split by |'`);
+  console.error(`Usage:\ncheck-files 'path/to/your/directory' 'path/to/directory to save the result file' 'file extension of files to search' 'searched expressions eg. "expression1|expression2"`);
   console.error('Will find searched expression in files with provided extension in the directory and save it in result file. If executed as a cron job, it will create a diff file with new files found since the last run.');
   process.exit(1);
 };
