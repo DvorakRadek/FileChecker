0c8ff0006aebfc7733a5e2d50fceafdf348c6b70
DvorakRadek <radekdvorak90@gmail.com>
2025-05-06 20:59:51 +0200

    updated readme and usage example

--- README.md
+++ README.md
@@ -4,9 +4,9 @@
 - find suspicious pieces of code in your files
 
 ## Installation
-- git clone https://github.com/DvorakRadek/FileChecker.git
-- cd FileChecker
-- npm install -g
+- npm i -g @dvorakradek/file-checker
 
-## Run
-- execute **check-files**
+## Usage
+- Will go through the directory, find all files with searched expression and save it into a result file. If executed as a cron job, it will create a diff file with new files found since the last run.
+- example: check-files /path/to/directory /path/to/result/directory php "expression1|expression2"
+- execute **check-files** --help for usage info
--- helpers.js
+++ helpers.js
@@ -1,6 +1,8 @@
 export const printHelpAndExit = () => {
   console.error(`Usage:\ncheck-files 'path/to/your/directory' 'path/to/directory to save the result file' 'file extension of files to search' 'searched expressions eg. "expression1|expression2"`);
-  console.error('Will find searched expression in files with provided extension in the directory and save it in result file. If executed as a cron job, it will create a diff file with new files found since the last run.');
+  console.log('Will find searched expression in files with provided extension in the directory and save it in result file. If executed as a cron job, it will create a diff file with new files found since the last run.');
+  console.log('Example:');
+  console.log('%c check-files\t/path/to/directory\t/path/to/result/directory\tphp\t"expression1|expression2"', 'color:green;');
   process.exit(1);
 };
 
